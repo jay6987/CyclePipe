b1939a2274ee60d0cdf4941efb37ec3b0973fd1f
黄俊杰 <jay6987@hotmail.com>
2023-04-23 23:41:37 +0800

    * fix build error in Windows

--- CyclePipe/Exception.h
+++ CyclePipe/Exception.h
@@ -6,9 +6,9 @@ namespace CyclePipe
     typedef  std::exception Exception;
     inline void ThrowExceptionAndLog(std::string&& msg) {
         std::cout<<msg;
-        std::__throw_logic_error(msg.c_str());
+        throw std::logic_error(msg.c_str());
     }
     inline void ThrowException(std::string&& msg) {
-        std::__throw_logic_error(msg.c_str());
+        throw std::logic_error(msg.c_str());
     }
 }
